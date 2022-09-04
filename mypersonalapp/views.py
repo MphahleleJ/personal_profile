74bc3feeabe62e10ad795fca7a8ac53fe9e922a1
June Mphahlele <june.mphahlele@gmail.com>
2022-09-05 00:58:01 +0200

    remove email

--- mypersonalapp/views.py
+++ mypersonalapp/views.py
@@ -4,18 +4,4 @@ from django.core.mail import send_mail
 
 # Create your views here.
 def home(request):
-    if request.method == "POST":
-        fullname= request.POST['fullname']
-        message_email =  request.POST['email']
-        content_message = request.POST['message']
-        
-        send_mail(
-            'Enquiry from ' + fullname,
-            content_message,
-            message_email,
-            ['june.mphahlele@gmail.com']
-        )
-        return render(request,'home.html',{'fullname':fullname})
-
-    else: 
         return render(request,'home.html',{})
