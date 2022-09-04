a04697d6a3534425cdbff329d7dda549c2fa53fb
June Mphahlele <june.mphahlele@gmail.com>
2022-09-04 23:00:13 +0200

    Add email functionality

--- mypersonalapp/views.py
+++ mypersonalapp/views.py
@@ -1,5 +1,21 @@
+from turtle import setundobuffer
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html',{})
+    if request.method == "POST":
+        fullname= request.POST['fullname']
+        message_email =  request.POST['email']
+        content_message = request.POST['message']
+        
+        send_mail(
+            'Enquiry from ' + fullname,
+            content_message,
+            message_email,
+            ['june.mphahlele@gmail.com']
+        )
+        return render(request,'home.html',{'fullname':fullname})
+
+    else: 
+        return render(request,'home.html',{})
